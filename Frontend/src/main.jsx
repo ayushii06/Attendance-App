3d9d878ec8ae642d8e1260bee439adfd467669d3
ayushi pal <ayushipal06@gmail.com>
2025-02-02 17:10:16 +0530

    add react-router-dom for routing functionality in the frontend

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,9 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-
 import { BrowserRouter } from 'react-router-dom';
-
 import { configureStore } from '@reduxjs/toolkit';
 import {Provider} from "react-redux"
 import rootReducer from './reducer';
