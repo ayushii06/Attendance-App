12cbb840ffc3a0eb89a6c5b3a1b8485754f7d986
ayushi pal <ayushipal06@gmail.com>
2025-03-27 21:40:05 +0530

    feat: update Procfile for improved Gunicorn configuration and add home route to Flask app

--- Python-api/app.py
+++ Python-api/app.py
@@ -274,6 +274,9 @@ def recognize_face():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
+@app.route('/')
+def home():
+    return "Hello, Flask on Azure!"
   
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
