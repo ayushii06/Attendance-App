b2033f967cd059becb4e3e7569a8a4847b98fdaa
ayushi pal <ayushipal06@gmail.com>
2025-03-31 20:03:49 +0530

    feat: implement Dockerfile for Python app and update default port in app.py

--- Python-api/app.py
+++ Python-api/app.py
@@ -279,5 +279,5 @@ def home():
     return "Hello, Flask on Azure!"
   
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # Default to 5000 if PORT is not set
+    port = int(os.environ.get("PORT", 80))  # Default to 5000 if PORT is not set
     app.run(host="0.0.0.0", port=port)
